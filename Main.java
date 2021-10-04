dca3df26c663990a8523ac215659b455ce7fabd6
Mothilal Rajendira Prasath <78716398+mothilalrajendiraprasath@users.noreply.github.com>
2021-10-04 19:37:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("REGEX USER VALIDATION");
+        Scanner sc= new Scanner(System.in);
+	com.company.userValidate validate = new com.company.userValidate();
+    System.out.println("Enter the first and last name:");
+    String flname = sc.nextLine();
+    validate.validateName(flname);
+        System.out.println("Enter the mail");
+        String mail= sc.nextLine();
+        validate.validateMail(mail);
+        System.out.println("Enter the phone number");
+        String num= sc.nextLine();
+        validate.validateNum(num);
+        System.out.println("Enter password");
+        String word= sc.nextLine();
+        validate.validatePassword(word);
+    }
+    }
+
---
+++ userValidate.java
@@ -0,0 +1,45 @@
+package com.company;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class userValidate {
+    public static Pattern p ;
+    public static Matcher m;
+
+    public void validateName(String flname){
+        Pattern p = Pattern.compile("^[A-Z]{1}[a-z]{2}\s[A-Z]{1}[a-z]{2}$");
+        Matcher m = p.matcher(flname);
+        boolean b= m.matches();
+        if(b==false){
+            System.out.println("name must start with capital and should not exceed more than 3 letter");
+        }
+    }
+
+    public void validateMail(String mail){
+        Pattern p = Pattern.compile("^[A-za-z.]+[A-za-z.]+@[a-z]{2}+.[a-z]{2}");
+        Matcher m = p.matcher(mail);
+        boolean b= m.matches();
+        if(b==false){
+            System.out.println("Check the mail");
+        }
+    }
+
+    public void validateNum(String num){
+        Pattern p = Pattern.compile("^[0-9]{2}[0-9]{10}$");
+        Matcher m = p.matcher(num);
+        boolean b= m.matches();
+        if(b==false){
+            System.out.println("phone number should be within 10 digit");
+        }
+    }
+
+    public void validatePassword(String word){
+        Pattern p = Pattern.compile("^[A-za-z0-9]{8}");
+        Matcher m = p.matcher(word);
+        boolean b= m.matches();
+        if(b==false){
+            System.out.println("Enter the right password");
+        }
+    }
+}
